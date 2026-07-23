3e506bbda0fc6053b7c29ae2cd9d8b2c72efab32
Phi Yen <hnpyen05.work@gmail.com>
2026-07-23 14:54:07 +0700

    Initial JavaFX project

---
+++ src/main/java/com/busbooking/BusTicketBooking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.busbooking;
+
+/**
+ *
+ * @author Phi Yen
+ */
+public class BusTicketBooking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
